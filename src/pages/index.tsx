ec84a9e56b62ec57a0dbb53a74eb22d06178fe81
priscila <priteodoro01@gmail.com>
2023-02-28 20:19:06 -0400

    Corrigindo Pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import CardList from "../components/CardList";
 import Header from "../components/Header";
-import Pagination from "../components/Pagination";
+import Pagination from "../components/Pagination"; 
+import styles from "../components/CardList";
 
 
 
@@ -8,25 +9,27 @@ import Pagination from "../components/Pagination";
 export default function Home() {
 
   const paginacao = [
-    {id: 1, link: "#", numero: 1 },
-    {id: 2, link: "#", numero: 2 },
-    {id: 3, link: "#", numero: 3 },
-    {id: 4, link: "#", numero: 4 },
+    { id: 1, link: "#", numero: 1 },
+    { id: 2, link: "#", numero: 2 },
+    { id: 3, link: "#", numero: 3 },
+    { id: 4, link: "#", numero: 4 },
   ]
 
   return (
-    <>      
-    <Header />
-        
-    <CardList />
-    
-    {paginacao.map(pagina =>(
-      <Pagination 
-      key={pagina.id}
-      link={pagina.link} 
-      numero={pagina.numero}/>
-    ))}
+    <>
+      <Header />
 
+    <div>
+        <CardList />
+        <div style={{ display: 'flex' }}>
+          {paginacao.map(pagina => (
+            <Pagination
+              key={pagina.id}
+              link={pagina.link}
+              numero={pagina.numero} />
+          ))}
+        </div>
+    </div>    
 
     </>
   )
