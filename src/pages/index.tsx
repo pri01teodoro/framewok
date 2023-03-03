f2352856050a49c576845a84a5ccee204873c5aa
priscila <priteodoro01@gmail.com>
2023-03-02 22:31:36 -0400

    Utilizando método useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import CardList from "../components/CardList";
 import Header from "../components/Header";
 import Pagination from "../components/Pagination"; 
 import styles from "../components/CardList/CardList.module.css";
+import Users from "../components/Users";
 /* import styles from "../components/Pagination/Pagination.module.css"; */
 
 
@@ -23,16 +24,16 @@ export default function Home() {
     <div className={styles.cardList}>
         <CardList />
         <div style={{ display: 'flex'}}>
-          <div>
           {paginacao.map(pagina => (
             <Pagination
               key={pagina.id}
               link={pagina.link}
               numero={pagina.numero} />
           ))}
-          </div>
         </div>
-    </div>    
+    </div>  
+
+    <Users />  
 
     </>
   )
