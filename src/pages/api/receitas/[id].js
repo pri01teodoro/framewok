867482e78e95d56c86585bd75c42d3bdd91815f6
priscila <priteodoro01@gmail.com>
2023-03-22 22:22:48 -0400

    Consertando dados.js

--- src/pages/api/receitas/[id].js
+++ src/pages/api/receitas/[id].js
@@ -1,8 +1,8 @@
 import { receitas } from "../../../../dados";
 
 export default function ReceitasId(req, res){
-    const {id} = req.query;
+    const { id } = req.query;
     const receita = receitas.find(r => r.id == id);
 
-    res.status(200).json(receita)
+    res.status(200).json(receita);
 }
--- src/pages/api/receitas/index.js
+++ src/pages/api/receitas/index.js
@@ -1,5 +1,6 @@
 import { receitas } from "../../../../dados";
 
 export default function Receitas(req, res){
+    
     return res.status(200).json(receitas)
 }
