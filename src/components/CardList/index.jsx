1bb4a10659196b7fba813ff4a4151000b061694b
priscila <priteodoro01@gmail.com>
2023-03-23 22:12:10 -0400

    Iniciando página de receita

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -3,26 +3,28 @@ import Card from "../Card";
 import styles from "../CardList/CardList.module.css";
 import axios from "axios";
 
-export default function CardList(){
+export default function CardList() {
 
     const [cards, setCards] = useState([]);
 
 
-    useEffect( () => {
+    useEffect(() => {
         axios.get('/api/receitas').then(resposta => setCards(resposta.data)); //data serve para pegar os dados
     }, []); //o [] vazio engloba tudo
 
 
-    return(
-      <div className={styles.cardList}>
-        {cards.map(receita => (
-            <Card 
-            key={receita.id}
-            titulo = {receita.titulo}
-            tempoPreparo = {receita.tempoPreparo}
-            porcoes = {receita.porcoes}
-            imagem = {receita.imagem}/>
-        ))}
+    return (
+        <div className={styles.cardList}>
+            {cards.map(receita => (
+                <Card
+                    key={receita.id}
+                    id={receita.id}
+                    titulo={receita.titulo}
+                    tempoPreparo={receita.tempoPreparo}
+                    porcoes={receita.porcoes}
+                    imagem={receita.imagem}
+                />
+            ))}
 
         </div>
     )
--- src/components/OpenRecipe/index.jsx
+++ src/components/OpenRecipe/index.jsx
@@ -5,34 +5,46 @@ import styles from "./OpenRecipe.module.css"
 
 import axios from "axios";
 
-export default function OpenRecipe(titulo){
+export default function OpenRecipe(titulo) {
 
     const router = useRouter();
-    const {id} = router.query
+    const { id } = router.query
 
 
     const [receita, setReceita] = useState
-    ({});
+        ({});
 
     useEffect(() => {
         //Consultar API
         axios.get(`/api/receitas/${id}`)
-        .then(resposta => setReceita
-            (resposta.data));
-    },[router])
+            .then(resposta => setReceita
+                (resposta.data));
+    }, [router])
 
     return (
         <>
+
             <div>
-                <div className={styles.container__title}>
-                    <Image 
-                        src={receita.imagem}
-                        alt={receita.descricao}
-                        width= "387"
-                        height= "297"
-                    />
-                </div>
+                <img className={styles.container__img} src={receita.capaImg} alt={receita.descricao} />
+                <h1 className={styles.box__title}>{receita.titulo}</h1>
             </div>
+            <main className={styles.container__main}>
+                <div className={styles.container__icon}>
+                    <div className={styles.icon}>
+                        <img src="/relogio.svg" alt="ícone de um relógio" />
+                        {receita.tempoPreparo} minutos
+                    </div>
+
+                    <div className={styles.icon}>
+                        <img src="/comida.svg" alt="ícone de um relógio" />
+                        {receita.porcoes} porções
+                    </div>
+                </div>
+            </main>
+
+
+
+
         </>
     )
 }
